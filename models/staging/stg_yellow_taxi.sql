d59c87b87ec55f27d6e12b2566af194a2492f92c
zeeniye <bluemul07@gmail.com>
2022-12-06 22:02:00 +0800

    raw data collections

---
+++ models/staging/stg_yellow_taxi.sql
@@ -0,0 +1,39 @@
+with table_joined as (
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-01') }}
+    UNION ALL 
+    SELECT *
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-02') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-03') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-04') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-05') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-06') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-07') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-08') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-09') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-10') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-11') }}
+    UNION ALL 
+    SELECT * 
+    FROM {{ source('nytaxi', 'yellow_tripdata_2021-12') }}
+)
+
+SELECT * FROM table_joined
