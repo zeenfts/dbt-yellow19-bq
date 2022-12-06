f619cc678041fb4a330681bfae1128797d1667ad
zeeniye <bluemul07@gmail.com>
2022-12-07 07:16:13 +0800

    fact tables

---
+++ models/warehouse/fact_trips.sql
@@ -0,0 +1,23 @@
+with yellow_taxi as (
+    SELECT * 
+    FROM {{ ref('stg_yellow_taxi') }}
+)
+
+SELECT
+    {{ dbt_utils.surrogate_key(['VendorID', 'tpep_pickup_datetime', 'PULocationID']) }} as tripId,
+    {{ dbt_utils.surrogate_key(['tpep_dropoff_datetime', 'trip_distance', 'RatecodeID', 'DOLocationID']) }} as paymentId,
+    VendorID as vendorId,
+    store_and_fwd_flag,
+    RatecodeID as rateCodeId,
+    PULocationID,
+    DOLocationID,
+    tpep_pickup_datetime,
+    tpep_dropoff_datetime,
+    payment_type,
+    passenger_count,
+    trip_distance,
+    total_amount
+FROM
+    yellow_taxi
+WHERE 
+    trip_distance > 0 AND passenger_count > 0
