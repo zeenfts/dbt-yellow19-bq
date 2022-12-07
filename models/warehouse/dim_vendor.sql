d3d08125ca3dbe7d7a884b45a64699b9b8b1900f
zeeniye <bluemul07@gmail.com>
2022-12-07 08:04:35 +0800

    vendor dim

---
+++ models/warehouse/dim_vendor.sql
@@ -0,0 +1,10 @@
+WITH vendor_stg AS(
+    SELECT DISTINCT 
+        VendorID
+    FROM {{ ref('stg_yellow_taxi') }}
+)
+
+SELECT 
+    VendorID,
+    decode_vendor(VendorID) as vendor_name
+FROM vendor_stg
