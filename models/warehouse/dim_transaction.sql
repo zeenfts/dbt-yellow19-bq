d73e931299639a1f888905c3f7c46218616796c3
zeeniye <bluemul07@gmail.com>
2022-12-07 08:12:06 +0800

    transaction dim select

---
+++ models/warehouse/dim_transaction.sql
@@ -0,0 +1,16 @@
+WITH yellow_taxi AS (
+    SELECT * 
+    FROM {{ ref('stg_yellow_taxi') }}
+)
+
+SELECT
+    {{ dbt_utils.surrogate_key(['tpep_dropoff_datetime', 'trip_distance', 'RatecodeID', 'DOLocationID']) }} as transactId,
+    fare_amount,
+    extra,
+    mta_tax,
+    tip_amount, 
+    tolls_amount,
+    improvement_surcharge,
+    congestion_surcharge
+FROM
+    yellow_taxi
